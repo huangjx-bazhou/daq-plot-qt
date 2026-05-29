7e63b239b68d40884ce738779330cb654b54d412
黄佳兴 <519207704@qq.com>
2026-05-29 15:41:28 +0800

    添加实验类

---
+++ Experiment.cpp
@@ -0,0 +1,80 @@
+﻿#include "Experiment.h"
+
+#include <QJsonValue>
+
+class ExperimentPrivate
+{
+public:
+    ExperimentPrivate() :
+        id(),
+        name()
+    {
+    }
+
+    QString id;
+    QString name;
+};
+
+Experiment::Experiment(QObject *parent) :
+    QObject(parent),
+    d(new ExperimentPrivate)
+{
+}
+
+Experiment::~Experiment()
+{
+}
+
+QString Experiment::id() const
+{
+    return d->id;
+}
+
+void Experiment::setId(const QString &id)
+{
+    if (d->id == id)
+    {
+        return;
+    }
+
+    d->id = id;
+    Q_EMIT idChanged(d->id);
+}
+
+QString Experiment::name() const
+{
+    return d->name;
+}
+
+void Experiment::setName(const QString &name)
+{
+    if (d->name == name)
+    {
+        return;
+    }
+
+    d->name = name;
+    Q_EMIT nameChanged(d->name);
+}
+
+QJsonObject Experiment::toJson() const
+{
+    QJsonObject json;
+    json.insert("id", d->id);
+    json.insert("name", d->name);
+
+    return json;
+}
+
+bool Experiment::fromJson(const QJsonObject &json)
+{
+    if (json.isEmpty())
+    {
+        return false;
+    }
+
+    this->setId(json.value("id").toString());
+    this->setName(json.value("name").toString());
+
+    return true;
+}
---
+++ Experiment.h
@@ -0,0 +1,62 @@
+﻿/**
+ * @file    Experiment.h
+ * @author  黄佳兴
+ * @version 0.1
+ * @date    2026-05-29
+ *
+ * @copyright Copyright (c) 2026 维拓启创 Inc. All rights reserved
+ */
+#ifndef EXPERIMENT_H
+#define EXPERIMENT_H
+
+#include <QObject>
+#include <QJsonObject>
+#include <QSharedPointer>
+
+QT_USE_NAMESPACE
+
+class ExperimentPrivate;
+
+/**
+ * @brief Experiment 表示项目中的一个实验
+ */
+class Experiment : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString id READ id WRITE setId NOTIFY idChanged)
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+
+public:
+    explicit Experiment(QObject *parent = Q_NULLPTR);
+    virtual ~Experiment();
+
+    /** 实验id */
+    QString id() const;
+
+    /** 设置实验id */
+    void setId(const QString &id);
+
+    /** 实验名称 */
+    QString name() const;
+
+    /** 设置实验名称 */
+    void setName(const QString &name);
+
+    /** 转换为Json对象 */
+    QJsonObject toJson() const;
+
+    /** 从Json对象加载 */
+    bool fromJson(const QJsonObject &json);
+
+Q_SIGNALS:
+    void idChanged(const QString &id);
+    void nameChanged(const QString &name);
+
+private:
+    Q_DISABLE_COPY(Experiment)
+    QScopedPointer<ExperimentPrivate> d;
+};
+
+using ExperimentSharedPointer = QSharedPointer<Experiment>;
+
+#endif // EXPERIMENT_H
