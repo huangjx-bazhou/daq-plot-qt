fdfe1061df11dbc193a6b06623fc040ffc729006
黄佳兴 <519207704@qq.com>
2026-05-29 15:20:08 +0800

    添加项目类

---
+++ Project.cpp
@@ -0,0 +1,74 @@
+﻿#include "Project.h"
+
+class ProjectPrivate
+{
+public:
+    ProjectPrivate() :
+        filePath(),
+        name(),
+        modified(false)
+    {
+    }
+
+    QString filePath;
+    QString name;
+    bool modified;
+};
+
+Project::Project(QObject *parent) :
+    QObject(parent),
+    d(new ProjectPrivate)
+{
+}
+
+Project::~Project()
+{
+}
+
+QString Project::filePath() const
+{
+    return d->filePath;
+}
+
+void Project::setFilePath(const QString &filePath)
+{
+    if (d->filePath == filePath)
+    {
+        return;
+    }
+
+    d->filePath = filePath;
+    Q_EMIT filePathChanged(d->filePath);
+}
+
+QString Project::name() const
+{
+    return d->name;
+}
+
+void Project::setName(const QString &name)
+{
+    if (d->name == name)
+    {
+        return;
+    }
+
+    d->name = name;
+    Q_EMIT nameChanged(d->name);
+}
+
+bool Project::isModified() const
+{
+    return d->modified;
+}
+
+void Project::setModified(bool modified)
+{
+    if (d->modified == modified)
+    {
+        return;
+    }
+
+    d->modified = modified;
+    Q_EMIT modifiedChanged(d->modified);
+}
---
+++ Project.h
@@ -0,0 +1,60 @@
+﻿/**
+ * @file    Project.h
+ * @author  黄佳兴
+ * @version 0.1
+ * @date    2026-05-29
+ *
+ * @copyright Copyright (c) 2026 维拓启创 Inc. All rights reserved
+ */
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <QObject>
+
+QT_USE_NAMESPACE
+
+class ProjectPrivate;
+
+/**
+ * @brief Project 表示软件当前打开的项目
+ */
+class Project : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString filePath READ filePath WRITE setFilePath NOTIFY filePathChanged)
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(bool modified READ isModified WRITE setModified NOTIFY modifiedChanged)
+
+public:
+    explicit Project(QObject *parent = Q_NULLPTR);
+    virtual ~Project();
+
+    /** 项目文件路径 */
+    QString filePath() const;
+
+    /** 设置项目文件路径 */
+    void setFilePath(const QString &filePath);
+
+    /** 项目名称 */
+    QString name() const;
+
+    /** 设置项目名称 */
+    void setName(const QString &name);
+
+    /** 项目是否被修改 */
+    bool isModified() const;
+
+    /** 设置项目是否被修改 */
+    void setModified(bool modified);
+
+Q_SIGNALS:
+    void filePathChanged(const QString &filePath);
+    void nameChanged(const QString &name);
+    void modifiedChanged(bool modified);
+
+private:
+    Q_DISABLE_COPY(Project)
+    QScopedPointer<ProjectPrivate> d;
+};
+
+#endif // PROJECT_H
